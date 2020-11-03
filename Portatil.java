361b4d2db39e0dd229cbf932ff125b03f14c8c93
Samuel <samualonso4@gmail.com>
2020-11-03 10:14:54 +0100

    Agregados los comentarios.

--- Portatil.java
+++ Portatil.java
@@ -4,34 +4,41 @@ public class Portatil {
     //El porcentaje de la batería.
     private int porcentajeBateria;
     //El portatil está encendido o no.
-    private boolean encendido;
+    private boolean encendido; 
     
+    //Este es el método constructor, el cual inicializa los atributos.
     public Portatil(String propietarioActual, int bateria) {
         propietario = propietarioActual;
         porcentajeBateria = bateria;
         encendido = true;
     }
     
+    //Este método devuelve el nombre del propietario.
     public String getPropietario() {
         return propietario;
     }
     
+    //Este método devuelve la batería que queda.
     public int getPorcentajeBateria() {
         return porcentajeBateria;
     }
     
+    //Este método indica si el portátil está encendido o apagado.
     public boolean getEncendido() {
         return encendido;
     }
     
+    //Este método permite cambiar el nombre al propietario.
     public void setPropietario(String Propietario) {
         this.propietario = Propietario;
     }
     
+    //Este método permite agregar cantidad de batería al portátil.
     public void cargarBateria(int porcentajeBateriaAñadido) {
         porcentajeBateria = porcentajeBateria + porcentajeBateriaAñadido;
     }
     
+    //Este método cambia el portátil de encendido a apagado y viceversa.
     public void botonEncendido() {
         if (encendido == true) {
             encendido = false;
@@ -41,10 +48,12 @@ public class Portatil {
         }
     }
     
+    //Este método imprime los detalles del portátil.
     public void imprimirTodosLosDetalles() {
         System.out.println(getTodosLosDetalles());
     }
     
+    //Este método devuelve todos los detalles del portátil.
     public String getTodosLosDetalles() {
         String estaEncendido = "no";
         if (encendido == true) {
